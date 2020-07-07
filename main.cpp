0eabbb891e714647160fcd626c4e152b3c909ab1
Duy Khoa <duykhoaptit@gmail.com>
2020-07-07 17:26:24 +0700

    update thêm + xoá máy bay (7-7-2020)

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ int main()
 	NODEMAYBAY nmb;
 	listMayBay lmb;
 	int n;
+	char s[20];
 	cout<<"So luong may bay can nhap: ";
 	cin>>n;
 	for (int i = 1; i<=n; i++)
@@ -25,6 +26,18 @@ int main()
 		cout<<"\n\nMay bay thu "<<i+1<<"!";
 		xuat(lmb.NodeMayBay[i]->data);
 	}
+//	cout<<"\n\nNhap vao so hieu may bay muon hieu chinh: ";
+//	fflush(stdin);
+//	gets(s);
+//	if (searchNodeMB(lmb,s) == -1) cout<<"Khong tim thay so hieu nay!";
+//	else updateNode(lmb,searchNodeMB(lmb,s));
+	//deleteNode(lmb,searchNodeMB(lmb,s));
+	
+//	for (int i = 0; i < lmb.n; i++)
+//	{
+//		cout<<"\n\nMay bay thu "<<i+1<<"!";
+//		xuat(lmb.NodeMayBay[i]->data);
+//	}
 	
 	return 0;
 }
